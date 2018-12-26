bd563a22a3d2be61d7c02dce8db2fa5313a1b574
Zach VanSomeren <zvansomeren@gmail.com>
2018-12-26 10:34:23 -0800

    Initial connect of Redux to React app

--- src/App.js
+++ src/App.js
@@ -5,26 +5,34 @@ import {
   Switch,
   Link,
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import logo from './logo.svg';
 import './App.css';
 
 import MoviesList from './MoviesList';
 import MovieDetail from './MovieDetail';
 
+const hello = () => ('hello');
+const store = createStore(hello);
+
 const App = () => (
-  <Router>
-    <div className="App">
-      <header className="App-header">
-        <Link to="/">
-          <img src={logo} className="App-logo" alt="logo" />
-        </Link>
-      </header>
-      <Switch>
-        <Route exact path="/" component={MoviesList} />
-        <Route path="/:id" component={MovieDetail} />
-      </Switch>
-    </div>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Link to="/">
+            <img src={logo} className="App-logo" alt="logo" />
+          </Link>
+        </header>
+        <Switch>
+          <Route exact path="/" component={MoviesList} />
+          <Route path="/:id" component={MovieDetail} />
+        </Switch>
+      </div>
+    </Router>
+  </Provider>
 );
 
 
